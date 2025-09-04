b68c7164bf92b96ced5f103e2d29ac9c71fb6d51
unknown <gofurovmuhammadsolih004@gmail.com>
2025-09-05 00:23:25 +0500

    changes: spring security is added in system

--- src/main/java/com/simpledev/dreamshops/security/user/ShopUserDetailsService.java
+++ src/main/java/com/simpledev/dreamshops/security/user/ShopUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.simpledev.dreamshops.security.user;
 
-public class ShopUserDetailsService {
+import com.simpledev.dreamshops.model.User;
+import com.simpledev.dreamshops.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ShopUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email)).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
 }
